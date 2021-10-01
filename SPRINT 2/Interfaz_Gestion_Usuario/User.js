125a78c213c2bfc1df39a1fb0f821db02de81b8e
JoanDMH <joandmh05@gmail.com>
2021-10-01 17:02:38 -0500

    Aplicación Web, sprint2

--- SPRINT 2/Interfaz_Gestion_Usuario/User.js
+++
@@ -1,8 +0,0 @@
-class User{
-    constructor(rol, nickname, email, password){
-        this.rol = rol
-        this.nickname = nickname
-        this.email = email
-        this.password = password
-    }
-}
