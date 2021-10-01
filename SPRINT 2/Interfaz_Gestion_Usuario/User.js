8d144ad1c437b89b4da005cdc5753ccc3071fa26
KevinAlex8 <90515679+KevinAlex8@users.noreply.github.com>
2021-09-30 21:59:12 -0500

    Add files via upload

---
+++ SPRINT 2/Interfaz_Gestion_Usuario/User.js
@@ -0,0 +1,8 @@
+class User{
+    constructor(rol, nickname, email, password){
+        this.rol = rol
+        this.nickname = nickname
+        this.email = email
+        this.password = password
+    }
+}
